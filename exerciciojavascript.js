de187038bae8bc978d25dad4091f99e10bb6d332
PatriciaPintor <153755504+PatriciaPintor@users.noreply.github.com>
2023-12-13 16:48:50 +0000

    Add files via upload

---
+++ exerciciojavascript.js
@@ -0,0 +1,33 @@
+// 1) Criar um objeto com o tema que vc preferi
+
+let house = {
+    collor: 'white',
+    bedrooms: 3,
+    toalet: 2,
+    living: 2,
+    sale: 200.000,
+    broker: 'PatriciaQA'
+}
+
+console.log(house)
+
+// 2) Criar um Array com uma lista de sua preferência e montar frases.
+
+let standcar = 'Stand Century';
+let product = 'Car';
+let model = 'BMW';
+let sale = ' from de 20000 Euros';
+let year = 2023;
+let client = 'Teacher Herzio';
+let seller = 'Claudemy';
+let collor = ['blue', 'red', 'black', 'white']
+
+
+console.log('The ' + standcar + ' did a big promotion '+ product + ' referent '+ model + sale + 
+            ' year of manufactur '+ year + ' ,sold to a customer of a company called Ryanair, '
+            + client + ' by an extraordinary seller ' + seller + ' , with a variety of colors '
+            + collor)
+
+
+
+
