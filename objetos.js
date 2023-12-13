2013f302a181deed78441677a3321c275a868451
PatriciaPintor <153755504+PatriciaPintor@users.noreply.github.com>
2023-12-13 16:45:28 +0000

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,25 @@
+let person = {
+    name: 'Patricia',
+    age: 46,
+    adress: 'Rua Bernardo Sequeira'
+}
+
+console.log(person)
+
+person.adress = 'Nothing Hill, Londres'
+
+console.log(person.adress)
+
+let celular = {
+    name: 'Iphone 15',
+    price: 1500,
+    color: 'red',
+    mark: 'Aplle'
+}
+
+console.log(celular)
+
+console.log('A maravilhosa '+ person.name + ' comprou um ' + celular.name + ' pela bagatela de '
+ + celular.price + ' Euros' + ' da marca ' + celular.mark + ' na cor ' + celular.color )
+
+
