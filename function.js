58b45f3208364bd8acfdc6bb5363f7933c6ece42
PatriciaPintor <153755504+PatriciaPintor@users.noreply.github.com>
2023-12-13 16:46:17 +0000

    Add files via upload

---
+++ function.js
@@ -0,0 +1,24 @@
+function helloWorld() {
+    console.log('Hello, World!')
+}
+helloWorld()
+
+function greeting(name) {
+    console.log('Hello, '+ name + '!');
+}
+//greeting('PatriciaQA')
+
+function cumprimento(firstName, lastName) {
+    console.log('Hello '+ firstName + lastName)
+}
+
+//cumprimento('Patricia', ' Pintor')
+
+function aoQuadrado (number) {
+    return number * number
+}
+
+let number = aoQuadrado(6)
+
+console.log(number)
+
