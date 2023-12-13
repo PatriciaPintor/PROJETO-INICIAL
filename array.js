269cae7d8877c14170047097c012f52fc15e8364
PatriciaPintor <153755504+PatriciaPintor@users.noreply.github.com>
2023-12-13 16:47:57 +0000

    Add files via upload

---
+++ array.js
@@ -0,0 +1,14 @@
+let collor = ['blue', 'yellow', 'black', 'brown', 'green']
+
+//console.log(collor)
+
+//console.log(collor[2])
+
+//console.log(collor.length)
+
+collor[5] = 'pink'
+
+console.log(collor)
+
+console.log(collor[1])
+
