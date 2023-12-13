375dab8a91196ed14d6c2b258ade56629abe0f22
PatriciaPintor <153755504+PatriciaPintor@users.noreply.github.com>
2023-12-13 16:41:21 +0000

    PROJETO JAVASCRIPT

---
+++ javascript.js
@@ -0,0 +1,40 @@
+//var nome = 'João';
+
+//let idade = 30;
+
+//const PI = 3.1416;
+
+//console.log(nome, idade, PI)
+
+//nome = 'Patricia'
+
+//console.log('Oi, meu nome é ' + nome + '!' )
+
+//let n1 = 5
+//let n2 = 4
+
+//console.log(n1 + n2)
+
+//let carroAzul = 'Megane';
+//let Carroazul = 'Fiat';
+//let CarroAzul = true;
+//let _carroazul = 30;
+//let $carroazul = ['Saveiro', 'Gol', 'BMW']
+
+//console.log(carroAzul, Carroazul, CarroAzul, _carroazul, $carroazul)
+
+let nome = 'Patricia';
+let idade = 46;
+let isApproved = true;
+let firstName;
+let lastName= null;
+
+console.log(nome)
+console.log(idade)
+console.log(isApproved)
+console.log(firstName)
+console.log(lastName)
+
+
+
+
